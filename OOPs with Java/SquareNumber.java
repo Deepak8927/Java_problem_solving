b9b8e4805b0f8d0a5a941899417700712cfd7a05
Deepak8927 <vijay577rana@gmail.com>
2026-04-12 13:25:36 +0530

    Feat: Added solution for Java problem 2.

---
+++ OOPs with Java/SquareNumber.java
@@ -0,0 +1,28 @@
+/*
+=========================================================================================
+Problem Statement 1:
+Write a program to print the square of a number passed through commandline arguments.
+=========================================================================================
+*/
+
+public class SquareNumber {
+    public static void main(String[] args) {
+        int num = Integer.parseInt(args[0]);
+        int square = num*num;
+
+        System.out.println("Square: " + square);
+    }
+
+}
+
+/*
+============================================================================
+Sample Execution in Terminal
+============================================================================
+cd <This file's path>
+javac SquareNumber.java
+java SquareNumber 12
+
+Square: 144
+============================================================================
+*/
