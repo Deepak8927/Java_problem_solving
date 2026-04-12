6070fbf33437015edc8468ce7d4dd566c219d1a5
Deepak8927 <vijay577rana@gmail.com>
2026-04-12 13:39:01 +0530

    Feat: Added solution of Java problem 3.

---
+++ OOPs with Java/WelcomeStudent.java
@@ -0,0 +1,28 @@
+/*
+=========================================================================================
+Problem Statement 3:
+Write a program to send the name and surname of a student through command line arguments
+and print a welcome message for the student.
+=========================================================================================
+*/
+
+public class WelcomeStudent {
+    public static void main(String[] args) {
+        String firstname = args[0];
+        String lastname = args[1];
+
+        System.out.println("Welcome, " + firstname + " " + lastname + "!");
+    }
+}
+
+/*
+============================================================================
+Sample Execution in Terminal
+============================================================================
+cd <This file's path>
+javac WelcomeStudent.java
+java WelcomeStudent Peter Parker
+
+Welcome, Peter Parker!
+============================================================================
+*/
