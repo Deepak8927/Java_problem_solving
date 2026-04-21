0de41b273038b0e06ce937585530e0ae679540f2
Deepak8927 <vijay577rana@gmail.com>
2026-04-21 15:52:23 +0530

    Feat: Added solution of Assignment-1 Problem-3.

---
+++ Assignment - 1/CelsiusToFahrenheit.java
@@ -0,0 +1,26 @@
+/*
+=========================================================================================
+Problem Statement 3:
+Write a Java program to change temperature from Celsius to Fahrenheit.
+=========================================================================================
+*/
+
+public class CelsiusToFahrenheit {
+    public static void main(String[] args) {
+        double celsius = 25;
+        double fahrenheit = (9.0/5)*celsius + 32;
+        System.out.println("Celsius: "+celsius);
+        System.out.println("Fahrenheit: "+fahrenheit);
+    }
+    
+}
+
+/*
+============================================================================
+Sample Execution in Terminal
+============================================================================
+Celsius: 25.0
+Fahrenheit: 77.0
+============================================================================
+*/
+
