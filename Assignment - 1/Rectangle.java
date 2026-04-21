0898999c57201ae38b1b48e7a06f4c961ecee680
Deepak8927 <vijay577rana@gmail.com>
2026-04-21 16:15:35 +0530

    Feat: Added solution of Assignment -1 Problem - 5.

---
+++ Assignment - 1/Rectangle.java
@@ -0,0 +1,28 @@
+/*
+=========================================================================================
+Problem Statement 5:
+Write a Java program to print your name.
+=========================================================================================
+*/
+
+class Rectangle {
+    public static void main(String[] args) {
+        double length = 10;
+        double breadth = 5;
+
+        double area = length * breadth;
+        double perimeter = 2 * (length + breadth);
+
+        System.out.println("Area of Rectangle: " + area);
+        System.out.println("Perimeter of Rectangle: " + perimeter);
+    }
+}
+
+/*
+============================================================================
+Sample Execution in Terminal
+============================================================================
+Area of Rectangle: 50.0
+Perimeter of Rectangle: 30.0
+============================================================================
+*/
