8cc8d5a2ae86e0e2caeaf757fab679fea52e7d83
Deepak8927 <vijay577rana@gmail.com>
2026-04-24 14:44:22 +0530

    Feat: Added solution of Java Assignment -1 problem 12.

---
+++ Assignment - 1/LeapYearChecker.java
@@ -0,0 +1,37 @@
+/*
+=========================================================================================
+Problem Statement 12:
+Write a Java program to check whether a year is leap year or not.
+=========================================================================================
+*/
+
+import java.util.Scanner;
+
+public class LeapYearChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = sc.nextInt();
+
+        if ((year%4 == 0 && year%100 != 0)||(year%400 == 0)){
+            System.out.println(year + " is a leap year.");
+        }
+        else {
+            System.out.println(year + " is not a leap year.");
+        }
+
+        sc.close();
+    }
+}
+
+/*
+============================================================================
+Sample Execution in Terminal
+============================================================================
+Enter a year: 2024
+2024 is a leap year.
+============================================================================
+*/
+
+
+
